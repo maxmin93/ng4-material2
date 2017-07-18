42510c24f1b612a40c89e9bfe4a3c2c06828c409
Byeong-Guk Min <maxmin93@gmail.com>
2017-07-18 18:37:51 +0900

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+// about Table
+import { DataSource } from '@angular/cdk';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/map';
+
+import { MdSnackBar } from '@angular/material';
+import { MdDialog } from '@angular/material';
+
+import { DialogExampleComponent } from './components/dialog-example/dialog-example.component';
+
+/** Constants used to fill up our data base. */
+const COLORS = ['maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple',
+  'fuchsia', 'lime', 'teal', 'aqua', 'blue', 'navy', 'black', 'gray'];
+const NAMES = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack',
+  'Charlotte', 'Theodore', 'Isla', 'Oliver', 'Isabella', 'Jasper',
+  'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'];
 
 @Component({
   selector: 'app-root',
@@ -7,4 +28,135 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  message = {
+  	type: 'NOTE',
+  	text: 'SnackBar Test Message'
+  };
+
+  // about Table
+  displayedColumns = ['userId', 'userName', 'progress', 'color'];
+  exampleDatabase = new ExampleDatabase();
+  dataSource: ExampleDataSource | null;
+
+  // about Slider
+	sliderValue = 50;
+
+  // about MdAutocomplete
+  stateCtrl: FormControl;
+  filteredStates: any;
+
+	// about MdAutocomplete
+  states = [
+    'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut',
+    'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa',
+    'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan',
+    'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+    'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio',
+  ];
+
+  // about Grid list
+  tiles = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
+
+  constructor(
+  	public snackBar: MdSnackBar,
+  	public dialog: MdDialog
+  ) {
+	  // about MdAutocomplete
+    this.stateCtrl = new FormControl();
+    this.filteredStates = this.stateCtrl.valueChanges
+        .startWith(null)
+        .map(name => this.filterStates(name));
+
+  }
+
+  ngOnInit() {
+  	// about Table
+    this.dataSource = new ExampleDataSource(this.exampleDatabase);
+  }
+
+  // about MdAutocomplete
+  filterStates(val: string) {
+    return val ? this.states.filter(s => s.toLowerCase().indexOf(val.toLowerCase()) === 0)
+               : this.states;
+  }
+
+  openSnackBar(message: any) {
+    this.snackBar.open(message.text, message.type, {
+      duration: 2000,
+    });
+  }
+
+  openDialog() {
+    this.dialog.open(DialogExampleComponent);
+  }
+
+};
+
+
+
+export interface UserData {
+  id: string;
+  name: string;
+  progress: string;
+  color: string;
 }
+
+/** An example database that the data source uses to retrieve data for the table. */
+export class ExampleDatabase {
+  /** Stream that emits whenever the data has been modified. */
+  dataChange: BehaviorSubject<UserData[]> = new BehaviorSubject<UserData[]>([]);
+  get data(): UserData[] { return this.dataChange.value; }
+
+  constructor() {
+    // Fill up the database with 100 users.
+    for (let i = 0; i < 100; i++) { this.addUser(); }
+  }
+
+  /** Adds a new user to the database. */
+  addUser() {
+    const copiedData = this.data.slice();
+    copiedData.push(this.createNewUser());
+    this.dataChange.next(copiedData);
+  }
+
+  /** Builds and returns a new User. */
+  private createNewUser() {
+    const name =
+        NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
+        NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
+
+    return {
+      id: (this.data.length + 1).toString(),
+      name: name,
+      progress: Math.round(Math.random() * 100).toString(),
+      color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
+    };
+  }
+}
+
+/**
+ * Data source to provide what data should be rendered in the table. Note that the data source
+ * can retrieve its data in any way. In this case, the data source is provided a reference
+ * to a common data base, ExampleDatabase. It is not the data source's responsibility to manage
+ * the underlying data. Instead, it only needs to take the data and send the table exactly what
+ * should be rendered.
+ */
+export class ExampleDataSource extends DataSource<any> {
+  constructor(private _exampleDatabase: ExampleDatabase) {
+    super();
+  }
+
+  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  connect(): Observable<UserData[]> {
+    return this._exampleDatabase.dataChange;
+  }
+
+  disconnect() {}
+}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
+
+// 이거 하나면 하위 모듈들 모두 커버 되는듯..
+import { MaterialModule } from '@angular/material';
+// import { MdButtonModule, MdCheckboxModule } from '@angular/material';		// 별도로 쓸만큼만 포함시킬 때
+import { MdAutocompleteModule, MdDatepickerModule, MdNativeDateModule, MdTableModule, } from '@angular/material';
+import { CdkTableModule } from '@angular/cdk';
 
 import { AppComponent } from './app.component';
+import { DialogExampleComponent } from './components/dialog-example/dialog-example.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogExampleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule,
+    FormsModule, ReactiveFormsModule, HttpModule,
+
+		MaterialModule, MdNativeDateModule, CdkTableModule,
+    // MdButtonModule, MdCheckboxModule, MdDatepickerModule, MdTableModule, MdAutocompleteModule    
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [
+  	DialogExampleComponent,
+  ],
+  bootstrap: [
+  	AppComponent
+  ]
 })
 export class AppModule { }
